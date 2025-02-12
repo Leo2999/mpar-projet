ae579b7fa7d32f71dda149ed50652fde1f79403a
Lucas Noronha <lucasnoronha7777@gmail.com>
2025-02-12 15:45:35 +0100

    Adding template of simulation classes

--- models.py
+++ models.py
@@ -3,6 +3,9 @@ class MarkovChain:
         self.states = []
         self.transitions = []
 
+    def build_simulation(self):
+        pass
+
 class MarkovDecisionProcess(MarkovChain):
     def __init__(self):
         super().__init__()
@@ -21,3 +24,14 @@ class TemporaryModel:
 
     def generate_model(self):
         pass
+
+# idea: use factory pattern to create the simulations
+class MCSimulation(MarkovChain):
+    def __init__(self, states, transitions):
+        self.states = states
+        self.transitions = transitions
+        self.actual_state = states[0]
+
+
+class MDPSimulation(MarkovDecisionProcess):
+    pass
