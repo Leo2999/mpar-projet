3b9550b4c8ec9d67fd60faf0e6bcc168b3f7a2fe
LeoRic <leo2999.ric@gmail.com>
2025-02-16 18:34:02 +0100

    merge done

--- mdp_leo.py
+++ mdp_leo.py
@@ -41,7 +41,6 @@ class gramPrintListener(gramListener):
 
         print("Transition from %s with no action and targets %s with weights %s" % (dep, ids, weights))   
 
-'''
 class MarkovGraph:
     def __init__(self, model):
         self.model = model
@@ -60,67 +59,9 @@ class MarkovGraph:
                 self.model.transitions_with_actions[i]["to"],
                 label=f'{self.model.transitions_with_actions[i]["action"]} ({self.model.transitions_with_actions[i]["weight"]})'
             )
-
-            #if self.model.transitions_with_actions[i]["actions"] is not None:
-             #   f.edge('model.transitions_with_actions[i]["from"]', 'model.transitions_with_actions[i]["to"]')
-    
     #creare un nodo intermedio per fare la biforcazione
         
         f.view()
-
-'''
-class MarkovGraph:
-    def __init__(self, model):
-        self.model = model
-
-    def plot(self):
-        f = Digraph('Markov', filename='Markov.gv')
-        f.attr(rankdir='LR', size='8,5')
-
-        f.attr('node', shape='circle')
-        for state in self.model.states:
-            f.node(state)
-
-        # Creiamo un dizionario per tracciare gli stati che hanno più azioni possibili
-        state_action_map = {}
-        for transition in self.model.transitions_with_actions:
-            state_action_map.setdefault(transition["from"], []).append(transition)
-
-        for state, transitions in state_action_map.items():
-            if len(transitions) > 1:
-                # Creiamo un nodo intermedio per la scelta
-                decision_node = f"choice_{state}"
-                f.node(decision_node, shape='circ', width="0")
-
-                # Collegamento dello stato originale al nodo decisionale
-                f.edge(state, decision_node)
-
-                # Collegamento del nodo decisionale alle varie opzioni con azioni e pesi
-                for transition in transitions:
-                    f.edge(
-                        decision_node,
-                        transition["to"],
-                        label=f'{transition["action"]} ({transition["weight"]})'
-                    )
-            else:
-                # Caso con una sola azione: collegamento diretto
-                transition = transitions[0]
-                f.edge(
-                    transition["from"],
-                    transition["to"],
-                    label=f'{transition["action"]} ({transition["weight"]})'
-                )
-
-        # Aggiungiamo anche le transizioni senza azione
-        for transition in self.model.transitions:
-            f.edge(
-                transition["from"],
-                transition["to"],
-                label=str(transition["weight"])
-            )
-
-        f.render(format='pdf', view=True)    # Genera e visualizza il grafico
-     
 
 def main():
     lexer = gramLexer(StdinStream())
