4aa5b1759a3ba633c8a340d35a472a849c350f65
LeoRic <leo2999.ric@gmail.com>
2025-03-08 18:27:40 +0100

    prova grafico

--- mdp2.py
+++ mdp2.py
@@ -1,15 +1,16 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+import time
+from gramLexer import gramLexer  # Assicurati che il file 'gramLexer.py' sia presente
+from gramParser import gramParser  # Assicurati che il file 'gramParser.py' sia presente
+from antlr4 import *
+from models import TemporaryModel, MarkovDecisionProcess
 from antlr4 import *
 from gramLexer import gramLexer
 from gramListener import gramListener
-from gramParser import gramParser
-import numpy as np
-from graphviz import Digraph
-from models import TemporaryModel, MarkovDecisionProcess
-from matplotlib import pyplot as plt    
-import matplotlib.image as mpimg
-import time
 
-        
 class gramPrintListener(gramListener):
 
     def __init__(self, model):
@@ -42,45 +43,49 @@ class gramPrintListener(gramListener):
         for i in range(len(ids)):
             self.model.transitions.append({'from': dep, 'to': ids[i], 'weight': weights[i]})
 
-        print("Transition from %s with no action and targets %s with weights %s" % (dep, ids, weights))   
+        print("Transition from %s with no action and targets %s with weights %s" % (dep, ids, weights))
+
 
 class MarkovGraph:
     def __init__(self, model):
         self.model = model
         self.current_edge = None
-        self.fp = Digraph('MarkovPath', filename='MarkovPath')
-        self.fp.attr(rankdir='LR', size='8,5')
-        self.fp.attr('node', shape='circle')
+        self.G = nx.DiGraph()  # Grafo diretto
         plt.ion()
         self.fig, self.ax = plt.subplots()
         self.plot_complete_graph()
 
     def plot_complete_graph(self):
-        self.fp.clear()
-        self.fp.attr(rankdir='LR', size='8,5')
-        self.fp.attr('node', shape='circle')
+        self.G.clear()  # Svuotiamo il grafo
 
         # Creiamo i nodi per tutti gli stati
         for state in self.model.states:
-            self.fp.node(state, style='filled', fillcolor='white')
+            self.G.add_node(state)
 
-        # Creiamo le transizioni senza azione
+        # Aggiungiamo le transizioni senza azione
         for transition in self.model.transitions:
-            self.fp.edge(transition['from'], transition['to'], label=str(transition['weight']), color='black')
+            self.G.add_edge(transition['from'], transition['to'], weight=transition['weight'], color='black')
 
-        # Se il modello è un MarkovDecisionProcess, creiamo anche le transizioni con azioni
+        # Se il modello è un MarkovDecisionProcess, aggiungiamo anche le transizioni con azioni
         if isinstance(self.model, MarkovDecisionProcess):
             for transition in self.model.action_transitions:
-                self.fp.edge(transition['from'], transition['to'], label=f"{transition['action']} ({transition['weight']})", color='black')
+                self.G.add_edge(transition['from'], transition['to'], weight=transition['weight'], color='black', action=transition['action'])
 
-        # Renderizziamo il grafico
-        self.fp.render(format='png')
         self.update_plot()
 
     def update_plot(self):
-        image = mpimg.imread('MarkovPath.png')
-        self.ax.clear()
-        self.ax.imshow(image)
+        # Impostiamo le posizioni dei nodi per evitare che si sovrappongano
+        pos = nx.spring_layout(self.G)
+
+        # Rimuoviamo gli archi esistenti per renderizzare il nuovo grafo
+        edge_colors = [self.G[u][v]['color'] for u, v in self.G.edges()]
+        weights = [self.G[u][v]['weight'] for u, v in self.G.edges()]
+
+        # Disegniamo i nodi e gli archi
+        nx.draw(self.G, pos, with_labels=True, node_size=5000, node_color='skyblue', font_size=10, font_weight='bold', ax=self.ax, edge_color=edge_colors, width=2)
+        nx.draw_networkx_edge_labels(self.G, pos, edge_labels={(u, v): f"{d['weight']}" for u, v, d in self.G.edges(data=True)}, ax=self.ax)
+
+        # Visualizziamo il grafico
         self.ax.axis('off')
         plt.draw()
         plt.pause(1)
@@ -93,45 +98,39 @@ class MarkovGraph:
         else:
             previous_state = None
 
-        self.fp.clear()  # Svuotiamo il grafico corrente
-        self.plot_complete_graph()  # Ripristiniamo il grafico di base
+        # Ripristiniamo il grafo completo
+        self.plot_complete_graph()
 
         # Evidenziamo lo stato corrente
-        self.fp.node(current_state, style='filled', fillcolor='yellow')
+        self.G.nodes[current_state]['color'] = 'yellow'
 
         # Se esiste una transizione precedente, la aggiungiamo
         if self.current_edge:
-            self.fp.edge(self.current_edge[0], self.current_edge[1], color='black', label=str(self.get_transition_weight(self.current_edge[0], self.current_edge[1])))
+            self.G[self.current_edge[0]][self.current_edge[1]]['color'] = 'black'
             self.current_edge = None
 
-        # Aggiungiamo solo l'ultima transizione in rosso, se esiste
+        # Aggiungiamo solo l'ultima transizione in rosso
         if previous_state:
             transition_found = False
             for transition in self.model.transitions:
                 if transition['from'] == previous_state and transition['to'] == current_state:
-                    # Prima rimuoviamo la freccia nera, se esiste
-                    self.fp.edge(transition['from'], transition['to'], color='white')  
-                    
-                    # Ora disegniamo la transizione in rosso
-                    self.fp.edge(previous_state, current_state, color='red', label=str(transition['weight']))
+                    # Rimuoviamo la freccia nera
+                    if self.G.has_edge(transition['from'], transition['to']):
+                        self.G[transition['from']][transition['to']]['color'] = 'white'  # Rimuoviamo l'arco disegnandolo bianco
+
+                    # Ora aggiungiamo la transizione rossa
+                    self.G.add_edge(previous_state, current_state, weight=transition['weight'], color='red')
                     self.current_edge = (previous_state, current_state)
                     transition_found = True
                     break
-            
+
             # Se non è stata trovata una transizione tra previous_state e current_state, non tracciarla
             if not transition_found:
                 print(f"Nessuna transizione trovata tra {previous_state} e {current_state}.")
-        
-        # Renderizziamo il grafico con la transizione rossa
-        self.fp.render(format='png')
+
+        # Rende di nuovo il grafico
         self.update_plot()
 
-    def get_transition_weight(self, from_state, to_state):
-        for transition in self.model.transitions:
-            if transition['from'] == from_state and transition['to'] == to_state:
-                return transition['weight']
-        return ''
-       
 def main():
     lexer = gramLexer(StdinStream())
     stream = CommonTokenStream(lexer)
@@ -145,21 +144,21 @@ def main():
     model = temp_model.generate_model()
     model.build_transition_matrix()
 
-    markov_graph = MarkovGraph(model) 
+    markov_graph = MarkovGraph(model)
     markov_graph.plot_complete_graph()
 
     if isinstance(model, MarkovDecisionProcess):
-            actions = model.simulation_init()
-            for _ in range(10):
-                if len(actions) == 0:
-                    _, actions = model.simulation_step(None)
-                    markov_graph.plot_simulation()
-                    time.sleep(1)
-                else:
-                    action = np.random.choice(list(actions))
-                    _, actions = model.simulation_step(action)
-                    markov_graph.plot_simulation()
-                    time.sleep(1)
+        actions = model.simulation_init()
+        for _ in range(10):
+            if len(actions) == 0:
+                _, actions = model.simulation_step(None)
+                markov_graph.plot_simulation()
+                time.sleep(1)
+            else:
+                action = np.random.choice(list(actions))
+                _, actions = model.simulation_step(action)
+                markov_graph.plot_simulation()
+                time.sleep(1)
     else:
         model.simulation_init()
         for _ in range(10):
